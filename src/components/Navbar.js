455d9608e570540e98cf4f04db5cdc386bf3c930
Tonywilson1211 <ant.wilson@hotmail.com>
2023-03-22 17:08:24 +0000

    add navbar elements to navbar.js and app.css

---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+
+    <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+        <div className="container">
+            <a class="navbar-brand" href="#">Anthony Wilson</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav ms-auto">
+                <li className="nav-item active">
+                    <a className="nav-link" href="#">Home <span className="sr-only"></span></a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Link</a>
+                </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
