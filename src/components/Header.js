a60fd1142d2b36aff2245c0b33a2fe08754cd520
Tonywilson1211 <ant.wilson@hotmail.com>
2023-03-23 12:37:17 +0000

    add background image to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,20 @@
 import React from "react";
+import Typed from "react-typed";
 
 const Header = () => {
   return (
     <div className="header-wraper">
         <div className="main-info">
-            <h1>web development and websites promotions</h1>
+            <h1>Anthony Wilson</h1>
+            <h2>Junior Software Developer</h2>
+            <Typed
+            className="typed-text"
+            strings={["HTML", "CSS", "JavaScript", "ReactJS", "Bootstrap", "Python"]}
+            typeSpeed={40}
+            backSpeed={40}
+            loop
+            />
+            <a href="#" className="btn-main-offer">Contact Me</a>
         </div>
     </div>
   )
