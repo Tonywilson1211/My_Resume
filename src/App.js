4ff9184e76bc5dcba8cd35a5944da749160bdc95
Tonywilson1211 <ant.wilson@hotmail.com>
2023-03-23 21:26:50 +0000

    add about me section

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 import Navbar from "./components/Navbar";
-import Header from "./components/Header"
+import Header from "./components/Header";
+import AboutMe from "./components/AboutMe";
 import Particle from './components/Particle';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Particle/>
       <Navbar/>
       <Header/>
+      <AboutMe/>
     </>
   );
 }
