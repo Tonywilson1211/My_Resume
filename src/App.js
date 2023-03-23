f5e8b5dd25a38e80e1925a11676e4247d5cd9505
Tonywilson1211 <ant.wilson@hotmail.com>
2023-03-23 13:24:31 +0000

    change particles from location

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 import Navbar from "./components/Navbar";
 import Header from "./components/Header"
-import Particles from 'react-tsparticles';
+import Particles from 'react-particles-js';
 
 function App() {
   return (
