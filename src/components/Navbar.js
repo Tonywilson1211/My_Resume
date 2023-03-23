300e60d4bb4417ad4eb1f8cf58120f9b87868b7b
Tonywilson1211 <ant.wilson@hotmail.com>
2023-03-23 10:52:45 +0000

    add navbar name, styling and font awesome

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   return (
@@ -7,17 +9,29 @@ const Navbar = () => {
         <div className="container">
             <a class="navbar-brand" href="#">Anthony Wilson</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
+                <FontAwesomeIcon icon={faBars} style={{ color: "#fff"}} />
             </button>
 
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav ms-auto">
-                <li className="nav-item active">
-                    <a className="nav-link" href="#">Home <span className="sr-only"></span></a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Link</a>
-                </li>
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Home <span className="sr-only"></span></a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">About</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Education</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Portfolio</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Contact</a>
+                    </li>
+                    <li className="nav-item" id="cv">
+                        <a className="nav-link" href="#">Download CV</a>
+                    </li>
                 </ul>
             </div>
         </div>
