cb75faaea323b35476f50914bb2ce45051abf73e
Tonywilson1211 <ant.wilson@hotmail.com>
2023-03-23 11:21:14 +0000

    add header wraper

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 import Navbar from "./components/Navbar";
+import Header from "./components/Header"
 
 function App() {
   return (
-    <Navbar></Navbar>
+    <>
+      <Navbar/>
+      <Header/>
+    </>
   );
 }
 
